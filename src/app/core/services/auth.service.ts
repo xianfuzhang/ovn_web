1aba7cd10176257b93e00bfde14ecb1915e90676
xianfu.zhang <286330850@qq.com>
2021-11-17 16:30:03 +0800

    登录成功授权

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -4,10 +4,8 @@ import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/htt
 import { Observable, of, Subject, throwError } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { User } from '../models/user';
-import { environment } from '../../../environments/environment';
 import { LoginService } from "../https/login";
 // import { CookieOptions, CookieService } from "ngx-cookie";
-// import { ApplicationService } from "./component/application.service";
 
 const APP_LOGIN_USER: string = 'app_login_user';
 export const APP_USER_GROUP: string = 'app_user_group';
@@ -16,53 +14,55 @@ export const APP_USER_GROUP: string = 'app_user_group';
   providedIn: 'root'
 })
 export class AuthService {
-  // private loggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  // private loginUser: User;
-  // public routeState = {};
+  private loginUser: string = '';
+  private isloggedIn: boolean = false;
 
   constructor(private router: Router,
-              // private cookieService: CookieService,
-              private loginService: LoginService,
-              // private applicationService: ApplicationService,
-              private http: HttpClient) {
+    // private cookieService: CookieService,
+    private loginService: LoginService) {
   }
 
   login(user: User): Observable<any> {
     return this.loginService.login({
       userName: user.userName,
       password: user.password
-    });
-    // .subscribe((res) => {
-    //   console.log(res);
-    // });
-      // .pipe(
-      //   take(1),
-      //   map((res: HttpResponse<any>) => {
-      //     // this.loggedIn$.next(true);
-      //     this.loginUser = {
-      //       userName: user.userName,
-      //       // group: res.groups[0]
-      //     }
-      //     localStorage.setItem(APP_LOGIN_USER, user.userName);
-      //     let group_mark = "1";
-      //     if (res.body.groups[0] == 'admingroup') {
-      //       group_mark = "3";
-      //     } else if (res.body.groups[0] == 'managergroup') {
-      //       group_mark = "2";
-      //     }
-      //     localStorage.setItem(APP_USER_GROUP, group_mark);
+    })
+      .pipe(
+        // take(1),
+        map((res: HttpResponse<any>) => {
+          console.log(res);
+          this.isloggedIn = true;
+          this.loginUser = user.userName;
+          localStorage.setItem(APP_LOGIN_USER, user.userName);
+          // let group_mark = "1";
+          // if (res.body.groups[0] == 'admingroup') {
+          //   group_mark = "3";
+          // } else if (res.body.groups[0] == 'managergroup') {
+          //   group_mark = "2";
+          // }
+          // localStorage.setItem(APP_USER_GROUP, group_mark);
 
-      //     let sessionId = res.headers.get('MARS_G_SESSION_ID');
-      //     let option: CookieOptions = {'path': "/"}
-      //     this.cookieService.put('marsGSessionId', sessionId, option);
-      //     return res;
-      //   }));
+          // let sessionId = res.headers.get('MARS_G_SESSION_ID');
+          // let option: CookieOptions = {'path': "/"}
+          // this.cookieService.put('marsGSessionId', sessionId, option);
+          return res;
+        }));
   }
-  
+
   logout() {
+    this.loginService.logout({userName: this.loginUser})
+      .subscribe((res) => {
+        console.log(res);
+      });
+    this.loginUser = '';
+    this.isloggedIn = false;
     localStorage.removeItem(APP_LOGIN_USER);
     // this.cookieService.remove('marsGSessionId');
     this.router.navigate(['/login']);
+  }
+
+  isUserLogin(): boolean {
+    return this.isloggedIn;
   }
 
 }
