23df92fa0f260506ddf99a4d11fc82a9ae9ffe6f
xianfu.zhang <286330850@qq.com>
2021-12-15 10:06:22 +0800

    mock device port and nat

--- devtools/mock/src/controllers/device.ts
+++ devtools/mock/src/controllers/device.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { CommonRoutes } from './route';
 const devices = require('../assets/ovn_devices.json');
 const links = require('../assets/ovn_links.json');
+const nats = require('../assets/ovn_nats.json');
 
 export class DeviceRoutes extends CommonRoutes {
   constructor(app: express.Application) {
@@ -17,6 +18,26 @@ export class DeviceRoutes extends CommonRoutes {
     this.app.route(`/ovn/v1/links`)
       .get((req: express.Request, res: express.Response) => {
         res.status(200).send(links);
+      });
+
+    this.app.route(`/ovn/v1/devices/:deviceId/ports`)
+      .get((req: express.Request, res: express.Response) => {
+        const deviceId = req.params.deviceId;
+        const device = devices["devices"].find((item: any) => item['uid'] === deviceId);
+        const result = {
+          ports: device['ports']
+        };
+        res.status(200).send(result);
+      });
+
+    this.app.route(`/ovn/v1/devices/:deviceId/nats`)
+      .get((req: express.Request, res: express.Response) => {
+        const deviceId = req.params.deviceId;
+        const natList = nats[deviceId] || [];
+        const result = {
+          nats: natList
+        };
+        res.status(200).send(result);
       });
 
     return this.app;
